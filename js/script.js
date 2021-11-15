3546e8504b3517d556bc767dcf12f8395f3e2e7a
Lynux2142 <lguiller@student.42.fr>
2021-11-15 13:36:48 +0100

    test2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+//import PHOTOS from '../photos/Budapest/photos.js';
+import { double } from './utils.js';
+
 document.getElementsByTagName('h1')[0].innerHTML += " World";
+console.log(double(5));
+/*
+let photos = document.getElementById('photos');
+
+PHOTOS.forEach(photo => {
+	let elem = document.createElement("img");
+	elem.src, photo.src;
+	photos.appendChild(elem);
+});
+*/
---
+++ photos/Budapest/photos.js
@@ -0,0 +1,20 @@
+const PHOTOS = [
+	{id: 1, src: './IMG_8117-2.jpg', title: 'photo1'},
+	{id: 2, src: './IMG_8121.jpg', title: 'photo2'},
+	{id: 3, src: './IMG_8134.jpg', title: 'photo3'},
+	{id: 4, src: './IMG_8155.jpg', title: 'photo4'},
+	{id: 5, src: './IMG_8198.jpg', title: 'photo5'},
+	{id: 6, src: './IMG_8213.jpg', title: 'photo6'},
+	{id: 7, src: './IMG_8247.jpg', title: 'photo7'},
+	{id: 8, src: './IMG_8262.jpg', title: 'photo8'},
+	{id: 9, src: './IMG_8295.jpg', title: 'photo9'},
+	{id: 10, src: './IMG_8310.jpg', title: 'photo10'},
+	{id: 11, src: './IMG_8317.jpg', title: 'photo11'},
+	{id: 12, src: './IMG_8348.jpg', title: 'photo12'},
+	{id: 13, src: './IMG_8361.jpg', title: 'photo13'},
+	{id: 14, src: './IMG_8378.jpg', title: 'photo14'},
+	{id: 15, src: './IMG_8412.jpg', title: 'photo15'},
+	{id: 16, src: './IMG_8442.jpg', title: 'photo16'},
+];
+
+export default PHOTOS;
