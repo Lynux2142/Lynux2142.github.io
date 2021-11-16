c174d62d12a91be4f145a2f4d8a5f295c6803939
Lynux2142 <lguiller@student.42.fr>
2021-11-16 16:57:11 +0100

    ajout d'un header et d'une barre de navigation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 import budapest from '/photos/Budapest/photos.js';
 
-document.getElementsByTagName('h1')[0].innerHTML += " World!";
 let photos = document.getElementById('photos');
 
 budapest.forEach(photo => {
 	let elem = document.createElement("img");
 	elem.setAttribute('src', photo.src);
-	console.log(EXIF.getData(photo, function() {
+	EXIF.getData(photo, function() {
 		let allMetaData = EXIF.getAllTags(this);
 		let ISOSpeedRatings = EXIF.getTag(this, 'ISOSpeedRatings');
 		let FNumber = EXIF.getTag(this, 'FNumber');
 		let ExposureTime = EXIF.getTag(this, 'ExposureTime');
 		let FocalLength = EXIF.getTag(this, 'FocalLength');
 		console.log(photo.src, ISOSpeedRatings, `F/${FNumber.numerator}`, `${ExposureTime.numerator}/${ExposureTime.denominator}s`, `${FocalLength.numerator}mm`);
-	}));
+	});
 	photos.appendChild(elem);
 });
